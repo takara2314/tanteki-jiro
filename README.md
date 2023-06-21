63ddf0c447339ca7b67a97909ffe6172f8e9a0fb
takara2314 <takara2314_TK@outlook.jp>
2023-06-21 09:10:45 +0900

    Add: 説明文 (README.md)

---
+++ README.md
@@ -0,0 +1,15 @@
+# 端的次郎
+
+文章を端的に、わかりやすくするわ。
+
+ChatGPT APIを使用したChrome拡張機能。
+
+## 前提
+
+- Node.js v16
+
+## ビルド
+
+```bash
+npm run build
+```
--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import { crx, defineManifest } from '@crxjs/vite-plugin';
 const manifest = defineManifest({
   manifest_version: 3,
   name: '端的次郎',
-  description: '文章を端的に。わかりやすくするわ。（ChatGPT API使用）',
+  description: '文章を端的に、わかりやすくするわ。（ChatGPT API使用）',
   version: '1.0.0',
   action: {
-		default_popup: 'index.html'
+    default_popup: 'index.html'
   },
   background: {
     service_worker: 'src/background.ts'
