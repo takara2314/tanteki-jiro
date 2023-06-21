3de0b92f26a1e20999280599a8670319eddaa407
takara2314 <takara2314_TK@outlook.jp>
2023-06-22 05:54:43 +0900

    Add: フッター

--- src/constants/tips.ts
+++ src/constants/tips.ts
@@ -1,1 +1,2 @@
 export const howToGetApiKeyPageUrl = 'https://laboratory.kazuuu.net/how-to-get-an-openai-api-key/';
+export const sourceCodePageUrl = 'https://github.com/takara2314/tanteki-jiro';
--- src/settings.tsx
+++ src/settings.tsx
@@ -1,7 +1,7 @@
 import { useCallback, ChangeEvent } from 'react';
 import { motion } from 'framer-motion';
 import { defaultSystemPrompt } from './constants/prompt';
-import { howToGetApiKeyPageUrl } from './constants/tips';
+import { howToGetApiKeyPageUrl, sourceCodePageUrl } from './constants/tips';
 
 interface Props {
   apiKey: string;
@@ -75,6 +75,21 @@ const Settings = ({ apiKey, systemPrompt, setApiKey, setSystemPrompt }: Props) =
           "
         />
       </div>
+
+      <footer className="my-3 text-center">
+        <small className="text-gray-500 text-base">
+          © 2023 Takara Hamaguchi
+        </small>
+        <div className="mt-1">
+          <a
+            href={sourceCodePageUrl}
+            target="_blank"
+            className="text-gray-500 hover:text-amber-700 text-sm transition-colors duration-100"
+          >
+            「端的次郎」のソースコード
+          </a>
+        </div>
+      </footer>
     </motion.section>
   );
 };
