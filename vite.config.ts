902a246d456ebf45abe736a348ed781db7432f3e
takara2314 <takara2314_TK@outlook.jp>
2023-06-22 06:19:28 +0900

    Add: アイコン

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,12 @@ const manifest = defineManifest({
   name: '端的次郎',
   description: '文章を端的に、わかりやすくするわ。（ChatGPT API使用）',
   version: '1.0.0',
+  icons: {
+    16: 'public/icon-16.png',
+    32: 'public/icon-32.png',
+    48: 'public/icon-48.png',
+    128: 'public/icon-128.png'
+  },
   action: {
     default_popup: 'index.html'
   },
